a7231881b7950e90151ad633c038a1ccc5425504
roshgupta <gpta.duke.rohan.1219@gmail.com>
2023-12-15 22:02:28 +0530

    made some basic UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,35 @@
 function App() {
-
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
+    <div className="grid grid-cols-12 divide-x divide-gray-300 ">
+      <aside className="col-span-3 h-screen overflow-y-auto">
+        {Array(50)
+          .fill(0)
+          .map((_, i) => {
+            return (
+              <div className="p-2 hover:bg-gray-200 cursor-pointer" key={i}>
+                Room #{i + 1}
+              </div>
+            );
+          })}
+      </aside>
+      <main className="col-span-9 px-8 h-screen overflow-y-auto flex flex-col">
+        <div className="flex-grow"></div>
+        <div className="mb-8 flex justify-center items-center gap-2">
+          <textarea
+            id="textarea"
+            name="textarea"
+            rows="2"
+            className="mb-8 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 flex-grow"
+          ></textarea>
+          <button
+            type="button"
+            class="flex-shrink-0 rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+          >
+            Send Message
+          </button>
+        </div>
+      </main>
+    </div>
   );
 }
 
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
+import forumPlugin from '@tailwindcss/forms'
 export default {
   content: ['./src/**/*.{js,jsx}'],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [forumPlugin],
 }
 
